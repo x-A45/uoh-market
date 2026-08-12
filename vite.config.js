ae59a16ab3afd3162db524511baf865b8de169cb
x-A45 <rathabhisek9308@gmail.com>
2026-08-13 00:36:19 +0530

    Fix GitHub Pages paths

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/uoh-market/'
-})
+  base: '/uoh-market/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        auth: resolve(__dirname, 'auth.html'),
+        account: resolve(__dirname, 'account.html'),
+        sell: resolve(__dirname, 'sell.html')
+      }
+    }
+  }
+});
